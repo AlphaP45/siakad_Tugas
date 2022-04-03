327bfcc5f4932b43eff2803009491e2ef1f9f405
AlphaP45 <alphapramadhani45@gmail.com>
2022-04-03 23:35:06 +0700

    Menambah Source Code Pada Controller Mahasiswa

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Mahasiswa;
+use Illuminate\Support\Facades\DB;
+
+class MahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //fungsi eloquent menampilkan data menggunakan pagination
+        // $mahasiswa = $mahasiswa = DB::table('mahasiswa')->get(); // Mengambil semua isi tabel
+
+        // Tugas Praktikum No 2
+        // Mengambil 3 data tabel
+        $mahasiswa = $mahasiswa = DB::table('mahasiswa')->paginate(3);
+
+        // Mengambil semua isi tabel
+        $posts = Mahasiswa::orderBy('nim', 'desc')->paginate(6);
+        return view('mahasiswa.index', compact('mahasiswa'))-> with('i', (request()
+        ->input('page', 1) - 1) * 5);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('mahasiswa.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //melakukan validasi data
+        $request->validate([
+            'nim' => 'required',
+            'nama' => 'required',
+            'kelas' => 'required',
+            'jurusan' => 'required',
+
+            // Tugas Praktikum No 1
+            'email' => ['required', 'email:dns'],
+            'alamat' => 'required',
+            'tanggal_lahir' => 'required',
+        ]);
+
+        //fungsi eloquent untuk menambah data
+        Mahasiswa::create($request->all());
+        //jika data berhasil ditambahkan, akan kembali ke halaman utama
+        return redirect()->route('mahasiswa.index')
+        ->with('success', 'Mahasiswa Berhasil Ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Mahasiswa $mahasiswa)
+    {
+        //menampilkan detail data dengan menemukan/berdasarkan Nim Mahasiswa
+        $Mahasiswa = $mahasiswa;
+        return view('mahasiswa.detail', compact('Mahasiswa'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Mahasiswa $mahasiswa)
+    {
+        //menampilkan detail data dengan menemukan berdasarkan Nim Mahasiswa untuk diedit
+         $Mahasiswa = $mahasiswa;
+        return view('mahasiswa.edit', compact('Mahasiswa'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Mahasiswa $mahasiswa)
+    {
+        //melakukan validasi data
+        $data= $request->validate([
+            'nim' => 'required',
+            'nama' => 'required',
+            'kelas' => 'required',
+            'jurusan' => 'required',
+
+            // Tugas Praktikum No 1
+            'email' => ['required', 'email:dns'],
+            'alamat' => 'required',
+            'tanggal_lahir' => 'required',
+        ]);
+        //fungsi eloquent untuk mengupdate data inputan kita
+        //memanggil nama kolom dalam model mahasiswa yang sesuai dengan id mahasiswa yg di req
+        Mahasiswa::where('id_mahasiswa', $mahasiswa->id_mahasiswa)->update($data);
+
+        //jika data berhasil diupdate, akan kembali ke halaman utama
+        return redirect()->route('mahasiswa.index')
+            ->with('success', 'Mahasiswa Berhasil Diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Mahasiswa $mahasiswa)
+    {
+        //fungsi eloquent untuk menghapus data
+        Mahasiswa::where('id_mahasiswa',$mahasiswa->id_mahasiswa)->delete();
+        return redirect()->route('mahasiswa.index')
+            -> with('success', 'Mahasiswa Berhasil Dihapus');
+    }
+
+    // Tugas Praktikum No 3
+    public function search(Request $request){
+        $keyword = $request -> search;
+        $mahasiswa = Mahasiswa::where('nama','like',"%". $keyword . "%") -> paginate(3);
+        return view(view: 'mahasiswa.index', data: compact( var_name:'mahasiswa'));
+    }
+}
